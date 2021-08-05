80837136561ccba4376be0b2c7b1c7543753b673
Eric Schleiff <ericschleiff@gmail.com>
2021-08-05 00:49:30 -0500

    first commit

---
+++ routes/index.js
@@ -0,0 +1,27 @@
+const express = require('express');
+const router = express.Router();
+const storeController = require('../controllers/storeControllers');
+const { catchErrors } = require('../handlers/errorHandlers');
+
+
+router.get('/', storeController.homePage);
+router.get('/stores', catchErrors(storeController.getStores));
+router.get('/add', storeController.addStore);
+router.post('/add',
+    storeController.upload,
+    catchErrors(storeController.resize),
+    catchErrors(storeController.createStore)
+);
+router.post('/add/:id',
+    storeController.upload,
+    catchErrors(storeController.resize),
+    catchErrors(storeController.updateStore)
+);
+router.get('/stores/:id/edit', catchErrors(storeController.editStore));
+router.get('/store/:slug', catchErrors(storeController.getStoreBySlug));
+router.get('/tags', catchErrors(storeController.getStoresByTag))
+router.get('/tags/:tag', catchErrors(storeController.getStoresByTag))
+//router.get('/login', userController.loginForm);
+
+module.exports = router;
+1
